f816f077aa875370ba7de9725a90dde56705bf2a
Frank <smbdman@hotmail.com>
2019-11-19 21:43:56 -0300

    exceções solução muito ruim

---
+++ OrientacaoObjetos/src/aplicacoes/ExcecoesPersonalizadas.java
@@ -0,0 +1,53 @@
+package aplicacoes;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import entidades.ExcecoesPersonalizadasReservas;
+
+
+public class ExcecoesPersonalizadas {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			System.out.println("Numero do quarto: ");
+			int numeroQuarto = sc.nextInt();
+			System.out.println("Entre data do checkIn(dd/mm/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.println("Entre data do checkOut(dd/mm/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+			if (checkIn.after(checkOut)) {
+				System.out.println("Erro a data de checkIn não pode ser posterior"
+						+ " a data de checkOut");
+			} else {
+				ExcecoesPersonalizadasReservas reserva = new ExcecoesPersonalizadasReservas(numeroQuarto, checkIn, checkOut);
+				System.out.println("Reserva: " + reserva);
+				
+				System.out.println();
+				System.out.println("Atualize as reservas: ");
+				System.out.println("Entre data do checkIn(dd/mm/yyyy): ");
+				checkIn = sdf.parse(sc.next());
+				System.out.println("Entre data do checkOut(dd/mm/yyyy): ");
+				checkOut = sdf.parse(sc.next());
+				
+				Date agora = new Date(); // retorna data de agora
+				
+				if ( checkIn.before(agora) || checkOut.before(agora) ) {
+					System.out.println("Data de checkIn ou checkOut não pode ser anterior a data atual");
+					
+				} else if(checkIn.after(checkOut)){
+					System.out.println("Data de checkIn não pode ser posterior a data de checkOut");
+				} else {
+					reserva.atualizaDatas(checkIn, checkOut);
+					System.out.println("Reserva: " + reserva);
+				}
+				
+			}
+			
+	}
+
+}
---
+++ OrientacaoObjetos/src/entidades/ExcecoesPersonalizadasReservas.java
@@ -0,0 +1,77 @@
+package entidades;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class ExcecoesPersonalizadasReservas {
+	
+	private Integer numeroQuarto;
+	private Date checkIn;
+	private Date checkOut;
+	
+	// Estando declarado como estatico não é instanciado a cada novo objeto e sim apenas
+	// uma vez, pois pertence a classe e não ao objeto
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	
+	public ExcecoesPersonalizadasReservas(Integer numeroQuarto, Date checkIn, Date checkOut) {
+		this.numeroQuarto = numeroQuarto;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	
+	
+	public Integer getNumeroQuarto() {
+		return numeroQuarto;
+	}
+
+
+
+	public void setNumeroQuarto(Integer numeroQuarto) {
+		this.numeroQuarto = numeroQuarto;
+	}
+
+
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+
+	public void atualizaDatas(Date checkIN, Date checkOUT) {
+		this.checkIn = checkIN;
+		this.checkOut = checkOUT;
+	}
+	
+	//Calculo para diferença entre duas datas
+	public Long duracao() {
+		// pega o valor da data em milissegundos e faz a diferença
+		long diferenca = checkOut.getTime() - checkIn.getTime();
+		//Esta classe do java TimeUnit é um tipo enumerado complexo com algumas operações
+		// neste caso vamos converter o valor em milissegundos da diferença em 
+		// dias, é possivel converter em horas, minutos...
+		return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS );
+	}
+
+
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Quarto: " + numeroQuarto + ", CheckIn: " + sdf.format(checkIn) +
+				", CheckOut: " + sdf.format(checkOut) + ", Dias: " +
+				this.duracao();
+	}
+	
+	
+	
+	
+}
